6769f9e05d80f1532552bb80f557e02fce710724
Max-Zhukovskyi <126571996+Max-Zhukovskyi@users.noreply.github.com>
2023-03-20 19:20:42 +0200

    Add files via upload

---
+++ 1-task/main.js
@@ -0,0 +1,17 @@
+const hi = prompt('Put birth year');
+const kal = 2023 - hi;
+alert('Your age ' + kal)
+
+const rad = prompt('Radius of circle ');
+const op = 3.14 * rad;
+alert('Your age ' + op + 'p');
+
+
+const km = prompt('km: ');
+const tm = prompt('time in hour: ');
+const all = km / tm;
+alert('time to arive' + all + 'km to hour');
+
+const dol = prompt('Dolars: ');
+const eur = dol * 1.07;
+alert('Your dolars ' + eur);
